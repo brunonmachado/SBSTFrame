9cb1d1e619c6ff9ae087f47fd4406b8e9ca587b2
Eduardo Horst <eduardoquijano2@gmail.com>
2016-04-04 16:21:13 -0300

    Correção de bug em acesso concorrente de CachedReport

--- src/sbstframe/problem/CachedProblem.java
+++ src/sbstframe/problem/CachedProblem.java
@@ -19,6 +19,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 
 /**
  * Class for storage of test results in order to not need re-tests.
@@ -71,16 +72,20 @@ public class CachedProblem extends ProblemDecorator {
         final Key currentKey;
         currentKey = new Key(testCase, testReq);
         
-        return cache.computeIfAbsent(currentKey, (Key myKey) -> {
-            while(cache.size() >= MAX_KEYS) removeRandomKey();
-            return problem.getTest(myKey.testCase, myKey.testReq);
+        return cache.computeIfAbsent(currentKey, new Function<Key, Boolean>() {
+            @Override
+            public synchronized Boolean apply(Key myKey) {
+                while(cache.size() >= MAX_KEYS) removeRandomKey();
+                return problem.getTest(myKey.testCase, myKey.testReq);
+                
+            }
         });
     }
     
     /**
      * Removes a random key from this cache
      */
-    private void removeRandomKey() {
+    private synchronized void removeRandomKey() {
         cache.remove(cache.keySet().stream().skip(rand.nextInt(cache.size())).findAny().get());
     }
 
